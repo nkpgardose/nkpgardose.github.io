bdcab04685999f2ba3bdaba37dd0fcb2bc3b155f
Neil Kim Gardose <nkpgardose@gmail.com>
2020-05-04 23:17:51 +1000

    update style, love acnh :heart:

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log("%cFeel free to visit my github page: \nhttps://github.com/nkpgardose.\nCheers 🍻", "color: rgb(240, 98, 146); font-size: 24px");
+console.log(
+  "%cFeel free to visit my github page: \nhttps://github.com/nkpgardose.\nCheers 🍻",
+  "color: rgb(240, 98, 146); font-size: 24px; font-family: 'Arial Rounded MT Bold';"
+);
+
+// const notifChat = document.querySelector("notif")[0]
+// notifChat.classList.remove("notif")
+
+function main(sleep) {
+  [1, 2, 3, 5, 6, 7, 9, 10].forEach(async (val) => {
+    await sleep(1500 * val);
+    const elem = document.querySelector(`[data-js-chat-${val}]`);
+    const notifElem = document.getElementsByClassName("notif")[0];
+    notifElem && notifElem.classList.remove("notif");
+    elem.classList.add("show", "notif");
+  });
+}
+
+const sleep = (time) =>
+  new Promise((resolve, reject) => {
+    setTimeout(resolve, time);
+  });
+
+main(sleep);
